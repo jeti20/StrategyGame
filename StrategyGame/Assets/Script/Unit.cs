ab204d167699be93f8fb8d0c9811dbad9556083f
Dehydrogenase <michalrodak98@gmail.com>
2024-11-17 00:42:41 +0100

    Added parametrs to animator and make script so when character is moving waling aniamtion works

--- StrategyGame/Assets/Script/Unit.cs
+++ StrategyGame/Assets/Script/Unit.cs
@@ -4,20 +4,27 @@ using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
+    [SerializeField] private Animator unitAnimator;
     private Vector3 targetPosition;
 
     private void Update()
-    {
+    {        
         float stoppingDistance = 0.1f;
-
         if (Vector3.Distance(transform.position, targetPosition) > stoppingDistance)
         {
+            //Movement
             Vector3 moveDirection = (targetPosition - transform.position).normalized;
             float moveSpeed = 4f;
-
             transform.position += moveDirection * moveSpeed * Time.deltaTime;
+
+        //Animations
+            unitAnimator.SetBool("IsWalking", true);
+        } else
+        {
+            unitAnimator.SetBool("IsWalking", false);
         }
 
+        //Movment func
         if (Input.GetMouseButtonDown(0))
         {
             Move(MouseWorld.GetPosition());
