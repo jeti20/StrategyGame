aa1b42bb33dd4cac9e28956e89946c03d7e77e22
Dehydrogenase <michalrodak98@gmail.com>
2024-11-17 12:41:08 +0100

    grid added

---
+++ StrategyGame/Assets/Script/Testing.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+    [SerializeField] private Transform gridDebugObjectPrefab;
+    private GridSystem gridSystem;
+
+    private void Start()
+    {
+       
+    }
+
+    private void Update()
+    {
+        
+    }
+
+}
--- StrategyGame/Assets/Script/Unit.cs
+++ StrategyGame/Assets/Script/Unit.cs
@@ -1,42 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//Handling moving, animations
+
 public class Unit : MonoBehaviour
 {
-    [SerializeField] private Animator unitAnimator;
-    private Vector3 targetPosition;
 
-    //Handling that the unit is standing where he is standing not 0,0,0. If delete this units will run to 0,0,0
-    private void Awake() 
+    [SerializeField] private Animator unitAnimator;
+
+
+    private Vector3 targetPosition;
+    private GridPosition gridPosition;
+
+    private void Awake()
     {
         targetPosition = transform.position;
     }
 
+    private void Start()
+    {
+        gridPosition = LevelGrid.Instance.GetGridPosition(transform.position);
+        LevelGrid.Instance.AddUnitAtGridPosition(gridPosition, this);
+    }
+
     private void Update()
-    {        
-        float stoppingDistance = 0.1f;
+    {
+
+        float stoppingDistance = .1f;
         if (Vector3.Distance(transform.position, targetPosition) > stoppingDistance)
         {
-            //Movement
             Vector3 moveDirection = (targetPosition - transform.position).normalized;
             float moveSpeed = 4f;
             transform.position += moveDirection * moveSpeed * Time.deltaTime;
 
-        //Animations
-        float rotateSpeed = 10f;
-        transform.forward = Vector3.Lerp(transform.forward, moveDirection, Time.deltaTime * rotateSpeed); //rotate when moving
+            float rotateSpeed = 10f;
+            transform.forward = Vector3.Lerp(transform.forward, moveDirection, Time.deltaTime * rotateSpeed);
+
             unitAnimator.SetBool("IsWalking", true);
-        } else
+        }
+        else
         {
             unitAnimator.SetBool("IsWalking", false);
         }
 
 
+        GridPosition newGridPosition = LevelGrid.Instance.GetGridPosition(transform.position);
+        if (newGridPosition != gridPosition)
+        {
+            // Unit changed Grid Position
+            LevelGrid.Instance.UnitMovedGridPosition(this, gridPosition, newGridPosition);
+            gridPosition = newGridPosition;
+        }
     }
 
     public void Move(Vector3 targetPosition)
     {
         this.targetPosition = targetPosition;
     }
+
 }
