8d988cc1231162f9b210d12f63eee88d96ef856c
Dehydrogenase <michalrodak98@gmail.com>
2024-11-17 09:33:49 +0100

    working visual selection (Observer pattern, singleton, Events)

--- StrategyGame/Assets/Script/UnitActionSystem.cs
+++ StrategyGame/Assets/Script/UnitActionSystem.cs
@@ -1,11 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
-//Handling movement, unit selection
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+//Handling movement, unit selection and unitselection visuals (observe and singletton pattern)
 public class UnitActionSystem : MonoBehaviour
 {
+    public static UnitActionSystem Instance {get; private set;} //get -> for other scripts to red, private set -> so other scripts cannot write
+        
+    public event EventHandler OnSelectedUnitChanged;
     [SerializeField] private Unit selectedUnit;
     [SerializeField] private LayerMask unitLayerMask;
+    
+    //Singleton
+    private void Awake()
+    {
+        if (Instance != null) 
+        {
+            Debug.LogError("There is more than one UnitActionSystem " + transform + " - " + Instance);
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+    }
     private void Update() 
     {      
         //Movment func
@@ -15,7 +35,7 @@ public class UnitActionSystem : MonoBehaviour
             selectedUnit.Move(MouseWorld.GetPosition()); 
         }
     }
-
+    //Unit selection
     private bool TryHandleUnitSelection()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -23,10 +43,27 @@ public class UnitActionSystem : MonoBehaviour
         {
             if (raycastHit.transform.TryGetComponent<Unit>(out Unit unit))
             {
-                selectedUnit = unit;
+                SetSelectedUnit(unit);
                 return true;
             }
         }
         return false;
+    }
+
+    //Visual Selection Effect - observer pattern
+    private void SetSelectedUnit(Unit unit)
+    {
+        selectedUnit = unit;
+        if (OnSelectedUnitChanged != null) // if true means there are subscribers
+        {
+            OnSelectedUnitChanged(this, EventArgs.Empty);
+        }
+        //OnSelectedUnitChange?Invoke(This, EventArgs.Empty); the same as the if
+    }
+
+    //making it accesable for other scripts to know what unit is selected
+    public Unit GetSelectedUnit()
+    {
+        return selectedUnit;
     }
 }
